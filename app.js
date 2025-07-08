d685993b3abc95d7a091c17eb01f34c18b087352
Nikhil Dubey <nikhildubey461@gmail.com>
2025-07-08 23:14:05 +0530

    Integrate user routes into the Express application by adding user route handling under the '/api' endpoint.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-
+import user from './src/routes/user.js'
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-    
+app.use('/api',user)
 
 export default app;
---
+++ src/routes/user.js
@@ -0,0 +1,4 @@
+import express from 'express'
+const router = express.Router()
+
+import User from '../controller/User.js'; router.use('/user',User)
